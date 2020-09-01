f82758ef147f7f0d78431a5ca8e4b89ad362caf7
void <git.email.address@googlemail.com>
2020-09-02 00:21:56 +0200

    Things.

---
+++ CrashAndBurn.Common/Stock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace CrashAndBurn.Common
+{
+    public class Stock
+    {
+        // Symbol or ISIN.
+        public string Id { get; private set; }
+
+        public List<StockData> History { get; private set; }
+
+        public Stock(string id, List<StockData> history)
+        {
+            Id = id;
+            History = history;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var stock = obj as Stock;
+            if (stock == null)
+            {
+                return false;
+            }
+            return Id == stock.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ CrashAndBurn.Common/StockMarket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CrashAndBurn.Common
+{
+    public class StockMarket
+    {
+        private HashSet<Stock> _Stocks = new HashSet<Stock>();
+
+        public StockMarket(IEnumerable<Stock> stocks)
+        {
+            foreach (var stock in stocks)
+            {
+                _Stocks.Add(stock);
+            }
+        }
+    }
+}
