ddbd85ddf26d38c9c49e9f1a6b25c8d58776365f
void <git.email.address@googlemail.com>
2020-09-01 22:02:33 +0200

    Downloader works.

--- CrashAndBurn.HistoryDownloader/Program.cs
+++ CrashAndBurn.HistoryDownloader/Program.cs
@@ -1,6 +1,6 @@
-﻿using CrashAndBurn.Common;
-using HtmlAgilityPack;
-using ScrapySharp.Extensions;
+﻿using AngleSharp;
+using CrashAndBurn.Common;
+using System;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -30,23 +30,31 @@ namespace CrashAndBurn.HistoryDownloader
             using (var webClient = new WebClient())
             {
                 string html = webClient.DownloadString(symbolUrl);
-                var document = new HtmlDocument();
-                document.LoadHtml(html);
-                var nodes = document.DocumentNode.CssSelect("#constituents td:first-child a.external");
+                var context = BrowsingContext.New(Configuration.Default);
+                var document = context.OpenAsync(request => request.Content(html)).Result;
+                var nodes = document.QuerySelectorAll("#constituents td:first-child a.external");
                 var symbolPattern = new Regex("^[A-Z]+$");
                 int counter = 1;
                 int symbolCount = nodes.Count();
                 foreach (var node in nodes)
                 {
-                    string symbol = node.InnerText;
-                    if (!symbolPattern.IsMatch(symbol))
-                    {
-                        continue;
-                    }
-                    string yahooUrl = $"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1=0&period2=2000000000&interval=1d&events=history";
+                    string symbol = node.TextContent;
                     string outputPath = Path.Combine(outputDirectory, $"{symbol}.csv");
-                    webClient.DownloadFile(yahooUrl, outputPath);
-                    Output.Write($"Downloaded {outputPath} ({counter}/{symbolCount})");
+                    try
+                    {
+                        if (!symbolPattern.IsMatch(symbol))
+                        {
+                            continue;
+                        }
+                        string yahooUrl = $"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1=0&period2=2000000000&interval=1d&events=history";
+                        webClient.DownloadFile(yahooUrl, outputPath);
+                        Output.WriteLine($"Downloaded {outputPath} ({counter}/{symbolCount})");
+                    }
+                    catch (Exception exception)
+                    {
+                        Output.Write($"Failed to download {outputPath} ({counter}/{symbolCount}): ");
+                        Output.WriteLine(exception.Message, ConsoleColor.Red);
+                    }
                     counter++;
                 }
             }
